dd515f2677a7321e13d051dd18b297dbf541e60a
Leo Dion <leogdion@brightdigit.com>
2023-01-09 19:42:15 -0500

    adding xcode project

---
+++ Jojo/JojoApp.swift
@@ -0,0 +1,17 @@
+//
+//  JojoApp.swift
+//  Jojo
+//
+//  Created by Leo Dion on 1/9/23.
+//
+
+import SwiftUI
+
+@main
+struct JojoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
