b33b1e3208aa47f7484efa5df12b237ed5be64b4
Leo Dion <leogdion@brightdigit.com>
2023-01-13 08:29:33 -0500

    refactored JWTBearerAuth

--- Packages/Jojo/Sources/JojoServer/TokenAuthenticator.swift
+++ Packages/Jojo/Sources/JojoServer/TokenAuthenticator.swift
@@ -73,113 +73,48 @@ extension JWTBearerAuthenicator {
     return try userID(fromToken: token)
   }
   
-  func respond(to request: Request, chainingTo next: AsyncResponder) async throws -> Response {
+  fileprivate func userFrom(bearer: BearerAuthorization, jwt: Request.JWT, using db: Database) async throws -> UserModel {
+    let userID = try self.userID(
+      fromAuthorization: bearer,
+      withJWT: jwt
+    )
+    
+    if let user = try await UserModel.find(userID, on: db) {
+      return user
+    }
+      let id = try tokenID(fromBearer: bearer)
+      
+      guard let token = try await TokenModel.find(id, on: db) else {
+        throw Abort(.unauthorized)
+      }
+      guard token.isValid  else {
+        throw Abort(.unauthorized)
+      }
+      let user = try await token[keyPath: TokenModel.userKey].get(on: db)
+      token[keyPath: TokenModel.updatedAtKey].timestamp = Date()
+      try await token.save(on: db)
+      return user
+    
+  }
+  
+  func userFrom(request: Request) async throws -> UserModel {
     
     guard let bearer = request.headers.bearerAuthorization else {
       throw Abort(.unauthorized)
     }
-
-    do {
-      let userID = try self.userID(
-        fromAuthorization: bearer,
-        withJWT: request.jwt
-      )
+    
+    return try await userFrom(bearer: bearer, jwt: request.jwt, using: request.db)
+  }
+  
+  func respond(to request: Request, chainingTo next: AsyncResponder) async throws -> Response {
+    
+    let user = try await userFrom(request: request)
+    
+    request.storage.set(UserKey.self, to: user)
+    return try await next.respond(to: request)
       
-      
-      guard let user = try await UserModel.find(userID, on: request.db) else {
-        throw Abort(.unauthorized)
-      }
-        
-      
-          request.storage.set(UserKey.self, to: user)
-      return try await next.respond(to: request)
-        
-    } catch {
-      let id = try tokenID(fromBearer: bearer)
-
-      guard let token = try await TokenModel.find(id, on: request.db) else {
-        throw Abort(.unauthorized)
-      }
-      let user = try await token[keyPath: TokenModel.userKey].get(on: request.db)
-      guard token.isValid  else {
-        throw Abort(.unauthorized)
-      }
-      request.storage.set(UserKey.self, to: user)
-      token[keyPath: TokenModel.updatedAtKey].timestamp = Date()
-      try await token.save(on: request.db)
-      return try await next.respond(to: request)
-    }
   }
 }
-
-//
-//struct TokenAuthenticator<UserType : Model, JWTTokenType: JWTPayload, TokenType: TokenAuthenticatable>: AsyncMiddleware  where TokenType.User == UserType{
-////  internal init(configuration: ApplicationConfiguration) {
-////    #if DEBUG
-////      skipAppleJWTVerification = configuration.skipAppleJWTVerification
-////    #endif
-////  }
-//
-////  #if DEBUG
-////    let skipAppleJWTVerification: Bool
-////  #endif
-//  struct UserKey: StorageKey {
-//    typealias Value = UserType
-//  }
-//
-//
-//  func userID(fromToken token: JWTTokenType) throws -> UserType.IDValue {
-//    //return UUID(token.sub.value)
-//    fatalError()
-//  }
-//
-//  func userID(fromAuthorization bearer: BearerAuthorization, withJWT jwt: Request.JWT) throws -> UserType.IDValue {
-//    let token = try jwt.verify(bearer.token, as: JWTTokenType.self)
-//    return try userID(fromToken: token)
-//  }
-//
-//  func tokenID(fromBearer bearer: BearerAuthorization) throws -> TokenType.IDValue {
-//    fatalError()
-//  }
-//
-//  func respond(to request: Request, chainingTo next: AsyncResponder) async throws -> Response {
-//
-//    guard let bearer = request.headers.bearerAuthorization else {
-//      throw Abort(.unauthorized)
-//    }
-//
-//    do {
-//      let userID = try self.userID(
-//        fromAuthorization: bearer,
-//        withJWT: request.jwt
-//      )
-//
-//
-//      guard let user = try await UserType.find(userID, on: request.db) else {
-//        throw Abort(.unauthorized)
-//      }
-//
-//
-//          request.storage.set(UserKey.self, to: user)
-//      return try await next.respond(to: request)
-//
-//    } catch {
-//      let id = try tokenID(fromBearer: bearer)
-//
-//      guard var token = try await TokenType.find(id, on: request.db) else {
-//        throw Abort(.unauthorized)
-//      }
-//      let user = try await token[keyPath: TokenType.userKey].get(on: request.db)
-//      guard token.isValid  else {
-//        throw Abort(.unauthorized)
-//      }
-//      request.storage.set(UserKey.self, to: user)
-//      token[keyPath: TokenType.updatedAtKey] = Date()
-//      try await token.save(on: request.db)
-//      return try await next.respond(to: request)
-//    }
-//  }
-//}
 
 
 struct TokenAuthenticator : JWTBearerAuthenicator{
